acfa6516b1ea086e0c1347510d38128fbf114826
Pablo Arranz Ropero <arranzropablo@gmail.com>
2018-05-07 19:05:40 +0200

    bayes arreglado maomenos

--- p3/bayes.m
+++ p3/bayes.m
@@ -1,25 +1,25 @@
-function [C1, C2] = bayes()
+function [C1, C2, verosimilitud] = bayes()
 
-  data = load("Iris2Clases.txt");
-  y = data(:,end);
-  data(:,end) = [];
-  X = data;
-  K = 2;
+    data = load("Iris2Clases.txt");
+    y = data(:,end);
+    data(:,end) = [];
+    X = data;
+    K = 2;
 
-  idx = find(y == 0);
-  X1 = X(idx, :);
-  idx = find(y == 1);
-  X2 = X(idx, :);
+    idx = find(y == 0);
+    X1 = X(idx, :);
+    idx = find(y == 1);
+    X2 = X(idx, :);
 
-  %Calculo de m
-  m1 = sum(X1) ./ rows(X1);
-  m2 = sum(X2) ./ rows(X2);
+    %Calculo de m
+    m1 = sum(X1) ./ rows(X1);
+    m2 = sum(X2) ./ rows(X2);
 
-  %Calculo de C
-  C1 = ((X1-m1)' * (X1 - m1)) ./ rows(X1);
-  C2 = ((X2-m2)' * (X2 - m2)) ./ rows(X2);
+    %Calculo de C
+    C1 = ((X1-m1)' * (X1 - m1)) ./ rows(X1);
+    C2 = ((X2-m2)' * (X2 - m2)) ./ rows(X2);
 
-%Cargamos los 3 datos de ejemplo y comprobamos a que clase pertenece cada uno
+    %Cargamos los 3 datos de ejemplo y comprobamos a que clase pertenece cada uno
     data = [];
     X = [];
     data(1,:) = load("TestIris01.txt");
@@ -30,7 +30,19 @@ function [C1, C2] = bayes()
     data(:,end) = [];
     X = data;
 
-%no se calcular la verosimilitud, de momento devuelvo los centros? (no se si tienen q dar 4x4)
-    verosimilitud = (1/((2*pi)* det(C1)^(1/2)))*exp((-1/2)*((X-m1) * pinv(C1)) * (X - m1)');
+    primeraparte = 1 / ( ((2*pi)^(columns(X)/2)) * (det(C1)^(1/2)));
+    segundaparte = exp( (-1/2)* (X(1,:) - m1) * pinv(C1) * (X(1,:) - m1)');
+    %no se calcular la verosimilitud, de momento devuelvo los centros? (no se si tienen q dar 4x4)
+    verosimilitud = primeraparte*segundaparte;
+
+    printf("verosimilitud clase 1: %f\n ", verosimilitud);
+
+    primeraparte = 1 / ( ((2*pi)^(columns(X)/2)) * (det(C2)^(1/2)));
+    segundaparte = exp( (-1/2)* (X(1,:) - m2) * pinv(C2) * (X(1,:) - m2)');
+    %no se calcular la verosimilitud, de momento devuelvo los centros? (no se si tienen q dar 4x4)
+    verosimilitud = primeraparte*segundaparte;
+
+    printf("verosimilitud clase 2: %f\n", verosimilitud);
+
 
 endfunction
